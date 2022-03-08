f087bb88ed75b182eaeb9a9957b2e285b0ec89cb
Nirmal <83758920+npatelGIT@users.noreply.github.com>
2022-03-07 19:39:36 -0500

    Added lottery program to test.py

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("Hello")
+def win_money(lottery_list):
+    import random
+    import collections
+    random_list = []
+    x = (random.randint(0, 9))
+    i = 1
+    while i < 2:
+        random_list.append(x)
+        i += 1
+    print("Winning Numbers:",lottery_list)  
+    print("My Random Numbers:",random_list)
+    if collections.Counter(random_list) == collections.Counter(lottery_list):
+        return(print("Congrats on winning $1,000,000!!!"))
+    else:
+        return(print("Aww...try again next time"))
