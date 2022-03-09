31aa6840ab4605b7e876bbbf5f2830365d80a894
Nirmal <83758920+npatelGIT@users.noreply.github.com>
2022-03-08 22:52:28 -0500

    Create amazon-products.py

---
+++ amazon-products.py
@@ -0,0 +1,34 @@
+import boto3
+import pandas
+
+# Creating the low level functional client
+client = boto3.client(
+    's3',
+    aws_access_key_id = 'AKIAZPDAWII47ZL7OMTV',
+    aws_secret_access_key = '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',
+    region_name = 'ap-south-1'
+)
+# Fetch the list of existing buckets
+clientResponse = client.list_buckets()
+
+""""  
+# Print the bucket names one by one
+print('Printing bucket names...')
+for bucket in clientResponse['Buckets']:
+    print(f'Bucket Name: {bucket["Name"]}')
+"""
+
+# Create the S3 object
+obj = client.get_object(
+    Bucket = 'mydeproject',
+    Key = 'Names.csv'
+)
+    
+# Read data from the S3 object
+data = pandas.read_csv(obj['Body'])
+    
+# Print the data frame
+print('Printing the data frame...')
+print(data)
+
+
